cbf5ccedd655281457226fdbd6e9d2ca428036af
shafeek <shafeek78@gmail.com>
2024-05-25 18:18:23 +0530

    first commit

---
+++ src/app/environment/environemnt.ts
@@ -0,0 +1,5 @@
+export const Environment = {
+    BASE_URL: 'http://localhost:8080',
+    ADMIN_URL : 'https://bknd.diebit.world/api/admin',
+    TRAINER_URL : 'https://bknd.diebit.world/api/trainer'
+}
